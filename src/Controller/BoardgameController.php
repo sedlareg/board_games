d618de86eb7824fed7fec80f9e85f09e9f9a718f
Geraldes <geraldes.pereira@con-vis.de>
2023-08-03 15:01:11 +0200

    added frontend pages

--- src/Controller/BoardgameController.php
+++ src/Controller/BoardgameController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Boardgame;
+use App\Repository\BoardgameRepository;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class BoardgameController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, BoardgameRepository $boardgameRepository): Response
     {
-        return $this->render('boardgame/index.html.twig', [
+        /*return $this->render('boardgame/index.html.twig', [
             'controller_name' => 'BoardgameController',
-        ]);
+        ]);*/
+        return new Response($twig->render('boardgame/index.html.twig', [
+            'boardgames' => $boardgameRepository->findAll(),
+        ]));
+    }
+
+    #[Route('/boardgame/{id}', name: 'boardgame')]
+    public function show(Environment $twig, Boardgame $boardgame, CommentRepository $commentRepository): Response
+    {
+        return new Response($twig->render('boardgame/show.html.twig', [
+            'boardgame' => $boardgame,
+            'comments' => $commentRepository->findBy(['boardgame' => $boardgame], ['createdAt' => 'DESC']),
+        ]));
     }
 }
