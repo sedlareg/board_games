db0cd22421dbbb76afce233464b683fe2256e634
Geraldes <geraldes.pereira@con-vis.de>
2023-08-03 15:13:14 +0200

    refactoring

--- src/Controller/BoardgameController.php
+++ src/Controller/BoardgameController.php
@@ -9,32 +9,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class BoardgameController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(Environment $twig, BoardgameRepository $boardgameRepository): Response
+    public function index(BoardgameRepository $boardgameRepository): Response
     {
         /*return $this->render('boardgame/index.html.twig', [
             'controller_name' => 'BoardgameController',
         ]);*/
-        return new Response($twig->render('boardgame/index.html.twig', [
+        return $this->render('boardgame/index.html.twig', [
             'boardgames' => $boardgameRepository->findAll(),
-        ]));
+        ]);
     }
 
     #[Route('/boardgame/{id}', name: 'boardgame')]
-    public function show(Request $request, Environment $twig, Boardgame $boardgame, CommentRepository $commentRepository): Response
+    public function show(Request $request, Boardgame $boardgame, CommentRepository $commentRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($boardgame, $offset);
 
-        return new Response($twig->render('boardgame/show.html.twig', [
+        return $this->render('boardgame/show.html.twig', [
             'boardgame' => $boardgame,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
-        ]));
+        ]);
     }
 }
