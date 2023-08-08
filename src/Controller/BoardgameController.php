c413488b40af4c24074544927250382baeb1803a
Geraldes <geraldes.pereira@con-vis.de>
2023-08-08 17:29:55 +0200

    added spam checker

--- src/Controller/BoardgameController.php
+++ src/Controller/BoardgameController.php
@@ -7,6 +7,7 @@ use App\Entity\Comment;
 use App\Form\CommentType;
 use App\Repository\BoardgameRepository;
 use App\Repository\CommentRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -18,7 +19,8 @@ class BoardgameController extends AbstractController
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-    ) {
+    )
+    {
     }
 
     #[Route('/', name: 'homepage')]
@@ -35,11 +37,13 @@ class BoardgameController extends AbstractController
     //#[Route('/boardgame/{id}', name: 'boardgame')]
     #[Route('/boardgame/{slug}', name: 'boardgame')]
     public function show(
-        Request $request,
-        Boardgame $boardgame,
-        CommentRepository $commentRepository,
+        Request                           $request,
+        Boardgame                         $boardgame,
+        CommentRepository                 $commentRepository,
+        SpamChecker                       $spamChecker,
         #[Autowire('%photo_dir%')] string $photoDir,
-    ): Response {
+    ): Response
+    {
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
 
@@ -48,12 +52,24 @@ class BoardgameController extends AbstractController
             $comment->setBoardgame($boardgame);
 
             if ($photo = $form['photo']->getData()) {
-                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
                 $photo->move($photoDir, $filename);
                 $comment->setPhotoFile($filename);
             }
 
             $this->entityManager->persist($comment);
+
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+
+            if ($spamChecker->getSpamScore($comment, $context) !== 0) {
+                throw new \RuntimeException('Blatant spam, go away!');
+            }
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('boardgame', ['slug' => $boardgame->getSlug()]);
